7c12225bff1858517d29079a5ef5e6bb0a2ac01d
Avi Dessauer <avi.the.coder@gmail.com>
2020-01-06 04:02:49 -0500

    Pass ghc-options to Ormolu

--- src/Haskell/Ide/Engine/Plugin/Ormolu.hs
+++ src/Haskell/Ide/Engine/Plugin/Ormolu.hs
@@ -10,9 +10,13 @@ import Haskell.Ide.Engine.MonadTypes
 import Control.Exception
 import Control.Monad.IO.Class ( liftIO , MonadIO(..) )
 import Data.Aeson ( Value ( Null ) )
-import Data.Text
+import Data.List
+import qualified Data.Text as T
 import Ormolu
 import Haskell.Ide.Engine.PluginUtils
+import Haskell.Ide.Engine.Cradle
+import HIE.Bios.Flags
+import HIE.Bios.Types
 #endif
 
 ormoluDescriptor :: PluginId -> PluginDescriptor
@@ -32,13 +36,24 @@ ormoluDescriptor plId = PluginDescriptor
 provider :: FormattingProvider
 provider _contents _uri _typ _opts =
 #if __GLASGOW_HASKELL__ >= 806
-  case _typ of 
-    FormatRange _ -> return $ IdeResultFail (IdeError PluginError (pack "Selection formatting for Ormolu is not currently supported.") Null)
+  case _typ of
+    FormatRange _ -> return $ IdeResultFail (IdeError PluginError (T.pack "Selection formatting for Ormolu is not currently supported.") Null)
     FormatText -> pluginGetFile _contents _uri $ \file -> do
-        result <- liftIO $ try @OrmoluException (ormolu defaultConfig file (unpack _contents))
+        cradle <- liftIO $ findLocalCradle file
+        res <- liftIO $ getCompilerOptions file cradle
+        let opts = case res of
+                    CradleSuccess o -> fmap DynOption $ filter exop $ componentOptions o
+                    _ -> []
+        let config = Config opts False False True False
+        result <- liftIO $ try @OrmoluException (ormolu config file (T.unpack _contents))
         case result of
-          Left  err -> return $ IdeResultFail (IdeError PluginError (pack $  "ormoluCmd: " ++ show err) Null)
+          Left  err -> return $ IdeResultFail (IdeError PluginError (T.pack $ "ormoluCmd: " ++ show err) Null)
           Right new -> return $ IdeResultOk [TextEdit (fullRange _contents) new]
 #else
   return $ IdeResultOk [] -- NOP formatter
 #endif
+  where
+    exop s =
+      "-X" `isPrefixOf` s
+        || "-fplugin=" `isPrefixOf` s
+        || "-pgmF=" `isPrefixOf` s
